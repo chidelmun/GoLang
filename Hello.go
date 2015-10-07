e7c605f1599bf32bc1a7fe7ee4cbe02a15226000
Delveri Chick <chickdelveri@gmail.com>
2015-10-07 17:24:08 +0100

    Go data types

--- Hello.go
+++ Hello.go
@@ -16,5 +16,10 @@ func main(){
 	var name string = "Dario"
 	word := "Some word" // implicit declaration of string
 
+	var age int = 25
+	var height float32 = 1.79
+	var weight uint8 = 97
+
 	fmt.Println("Here we go", name, word)
+	fmt.Println("You are ", age , height , weight  )
 }
