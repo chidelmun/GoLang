7666df6e7c9a6e27664a5ff4a94978309d7db5c5
Delveri Chick <chickdelveri@gmail.com>
2015-11-29 17:38:43 +0100

    Using the const key word

--- Hello.go
+++ Hello.go
@@ -11,24 +11,9 @@ import "fmt"
 
 
 func main(){
-	// Go data types
+	
+	const Name string = "Dario"
 
-	var name string = "Dario"
-	word := "Some word" // implicit declaration of string
-
-	var age int = 25
-	var height float32 = 1.79
-	var weight uint8 = 97
-	var one,two,three int
-	one = 1
-	two = 2
-	three = 3
-
-	var a,b,c = 22,44,"word"
-	fmt.Println(a,b,c)
-
-	fmt.Println("Here we go", name, word)
-	fmt.Println("You are ", age , height , weight, one,two,three  )
-
-	fmt.Printf("%T, %T, %T\n", age, height, weight)
+	const PI float32 = 3.1415
+	fmt.Println(Name, PI)
 }
